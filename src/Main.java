61a775f2bff6d5fcb5eac486d55f80e2b3ec6696
Forestjaba <skartem0106@mail.ru>
2023-12-13 03:29:47 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите количество строк: ");
+        int n = Integer.parseInt(scanner.nextLine());
+        List<String> strings = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Введите строку " + (i + 1) + ": ");
+            strings.add(scanner.nextLine());
+        }
+
+        Collections.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if (s1.length() != s2.length()) {
+                    return s1.length() - s2.length();
+                } else {
+                    return s1.compareTo(s2);
+                }
+            }
+        });
+
+        System.out.println("Отсортированные строки: ");
+        for (String str : strings) {
+            System.out.println(str);
+        }
+    }
+}
